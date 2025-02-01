fdeedd65a8fec0143b34fce9a0925b55b0c66f83
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2025-02-01 16:54:46 +0100

    set lint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import pluginReact from "eslint-plugin-react";
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist', '*.config.{js,mjs,ts}'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended, pluginReact.configs.flat.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react': pluginReact,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-unused-vars': ['error', {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_'
+      }],
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+  },
+)
