749a9797c8a74e9bffddea79be4070285eb1033d
Maciej Jastrzebski <mdjastrzebski@gmail.com>
2026-04-11 17:29:40 +0200

    feat: elevent labs tts (requires API key)

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './app';
+import { initializeSpeech } from './utils/speech-service';
+
+initializeSpeech();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
